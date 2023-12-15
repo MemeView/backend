a0c994da221a994aa86222ac2b4e7525599d9b71
Zaur Dedegkaev <dedegkaev.z@gmail.com>
2023-12-15 20:07:48 +0300

    Snapshot

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DefinedTokensModule } from './defined-tokens/defined-tokens.module';
-import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: true,
-    }),
     DefinedTokensModule,
   ],
   controllers: [AppController],
--- src/graphql/getFilterTokens.ts
+++ src/graphql/getFilterTokens.ts
@@ -1,204 +1,193 @@
-import { gql } from '@apollo/client';
 
-export const GET_FILTER_TOKENS = gql`
-  query GetPairMetadata(
-    $tokens: [String]
-    $filters: TokenFilters
-    $limit: Int
-    $offset: Int
-    $phrase: String
-  ) {
-    filterTokens(
-      tokens: $tokens
-      filters: $filters
-      phrase: $phrase
-      limit: $limit
-      offset: $offset
-    ) {
-      count
-      results {
-        quoteToken
-        buyCount1
-        buyCount12
-        buyCount24
-        buyCount4
-        uniqueBuys1
-        uniqueBuys12
-        uniqueBuys24
-        uniqueBuys4
-        change1
-        change12
-        change24
-        change4
-        high1
-        high12
-        high24
-        high4
-        lastTransaction
-        liquidity
-        low1
-        low12
-        low24
-        low4
-        marketCap
-        priceUSD
-        quoteToken
-        sellCount1
-        sellCount12
-        sellCount24
-        sellCount4
-        uniqueSells1
-        uniqueSells12
-        uniqueSells24
-        uniqueSells4
-        txnCount1
-        txnCount12
-        txnCount24
-        txnCount4
-        uniqueTransactions1
-        uniqueTransactions12
-        uniqueTransactions24
-        uniqueTransactions4
-        volume1
-        volume12
-        volume24
-        volume4
-        pair {
-          address
-          token0Data {
-            name
-            info {
-              imageLargeUrl
-              address
-              circulatingSupply
-              cmcId
-              id
-              imageSmallUrl
-              imageThumbUrl
-              isScam
-              name
-              networkId
-              symbol
-              totalSupply
-            }
-            address
-            cmcId
-            decimals
-            exchanges {
-              address
-              color
-              exchangeVersion
-              iconUrl
-              id
-              name
-              networkId
-              tradeUrl
-            }
-            id
-            isScam
-            networkId
-            socialLinks {
-              bitcointalk
-              blog
-              coingecko
-              coinmarketcap
-              discord
-              email
-              facebook
-              github
-              instagram
-              linkedin
-              reddit
-              slack
-              telegram
-              twitch
-              twitter
-              website
-              wechat
-              whitepaper
-              youtube
-            }
-            symbol
-            totalSupply
-          }
-          token1Data {
-            name
-            address
-            cmcId
-            decimals
-            exchanges {
-              address
-              color
-              exchangeVersion
-              iconUrl
-              id
-              name
-              networkId
-              tradeUrl
-            }
-            explorerData {
-              blueCheckmark
-              description
-              divisor
-              id
-              tokenPriceUSD
-              tokenType
-            }
-            id
-            info {
-              address
-              circulatingSupply
-              cmcId
-              id
-              imageLargeUrl
-              imageSmallUrl
-              imageThumbUrl
-              isScam
-              name
-              networkId
-              symbol
-              totalSupply
-            }
-            isScam
-            networkId
-            symbol
-            totalSupply
-          }
-          exchangeHash
-          fee
-          id
-          networkId
-          tickSpacing
-          token0
-          token1
+export const GET_FILTER_TOKENS = `query FilterTokens($tokens: [String], $limit: Int, $offset: Int, $rankings: [TokenRanking], $filters: TokenFilters) {
+  filterTokens(tokens: $tokens, limit: $limit, offset: $offset, filters: $filters, rankings: $rankings) {
+    page
+    count
+    results {
+      quoteToken
+      createdAt
+      buyCount1
+      buyCount12
+      buyCount24
+      buyCount4
+      uniqueBuys1
+      uniqueBuys12
+      uniqueBuys24
+      uniqueBuys4
+      change1
+      change12
+      change24
+      change4
+      high1
+      high12
+      high24
+      high4
+      lastTransaction
+      liquidity
+      low1
+      low12
+      low24
+      low4
+      marketCap
+      priceUSD
+      quoteToken
+      sellCount1
+      sellCount12
+      sellCount24
+      sellCount4
+      uniqueSells1
+      uniqueSells12
+      uniqueSells24
+      uniqueSells4
+      txnCount1
+      txnCount12
+      txnCount24
+      txnCount4
+      uniqueTransactions1
+      uniqueTransactions12
+      uniqueTransactions24
+      uniqueTransactions4
+      volume1
+      volume12
+      volume24
+      volume4
+      token {
+        totalSupply
+        networkId
+        address
+        decimals
+        id
+        name
+        info {
+          circulatingSupply
+          imageLargeUrl
+          imageSmallUrl
         }
-        exchanges {
-          address
-          color
-          exchangeVersion
-          id
-          name
-          networkId
-          tradeUrl
-          iconUrl
-        }
-        token {
-          totalSupply
-          networkId
-          address
-          decimals
-          id
+        networkId
+        isScam
+        symbol
+      }
+      pair {
+        address
+        token0Data {
           name
           info {
+            imageLargeUrl
+            address
             circulatingSupply
+            cmcId
+            id
+            imageSmallUrl
+            imageThumbUrl
+            isScam
+            name
+            networkId
+            symbol
+            totalSupply
+          }
+          address
+          cmcId
+          decimals
+          exchanges {
+            address
+            color
+            exchangeVersion
+            iconUrl
+            id
+            name
+            networkId
+            tradeUrl
+          }
+          id
+          isScam
+          networkId
+          socialLinks {
+            bitcointalk
+            blog
+            coingecko
+            coinmarketcap
+            discord
+            email
+            facebook
+            github
+            instagram
+            linkedin
+            reddit
+            slack
+            telegram
+            twitch
+            twitter
+            website
+            wechat
+            whitepaper
+            youtube
+          }
+          symbol
+          totalSupply
+        }
+        token1Data {
+          name
+          address
+          circulatingSupply
+          cmcId
+          decimals
+          exchanges {
+            address
+            color
+            exchangeVersion
+            iconUrl
+            id
+            name
+            networkId
+            tradeUrl
+          }
+          explorerData {
+            blueCheckmark
+            description
+            divisor
+            id
+            tokenPriceUSD
+            tokenType
+          }
+          id
+          info {
+            address
+            circulatingSupply
+            cmcId
+            id
             imageLargeUrl
             imageSmallUrl
+            imageThumbUrl
+            isScam
+            name
+            networkId
+            symbol
+            totalSupply
           }
-          networkId
           isScam
+          networkId
           symbol
+          totalSupply
         }
+        exchangeHash
+        fee
+        id
+        networkId
+        tickSpacing
+        token0
+        token1
+      }
+      exchanges {
+        address
+        color
+        exchangeVersion
+        id
+        name
+        networkId
+        tradeUrl
+        iconUrl
       }
     }
   }
+}
 `;
