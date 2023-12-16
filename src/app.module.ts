04c214d605395b0b929176711f01d76a7d70a3a0
Zaur Dedegkaev <dedegkaev.z@gmail.com>
2023-12-16 14:08:01 +0300

    Snapshot

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
 import { DefinedTokensModule } from './cron/defined-tokens/defined-tokens.module';
-import { ScheduleModule } from '@nestjs/schedule';
 import { CronModule } from './cron/cron.module';
 import { VolumeModule } from './cron/volume-sync/volume.module';
 import { SolveScoreModule } from './cron/solve-score-sync/solve-score.module';
 
 @Module({
-  imports: [CronModule, DefinedTokensModule, SolveScoreModule, VolumeModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'],
+    }),
+    CronModule,
+    DefinedTokensModule,
+    SolveScoreModule,
+    VolumeModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
