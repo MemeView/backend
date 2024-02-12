74a93060d0f7a67908f3be5036d4dd5c0e00b912
Zaur Dedegkaev <dedegkaev.z@gmail.com>
2024-02-12 19:56:08 +0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import 'graphql-import-node';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const now = new Date();
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
